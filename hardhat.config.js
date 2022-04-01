149fa211a8c5886d159a8ca97fbc137e5880235c
Milos Pavlovic <milosar7@gmail.com>
2022-04-01 16:57:20 +0200

    Mainnet accout

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,18 @@
-require("@nomiclabs/hardhat-waffle");
+import "@nomiclabs/hardhat-waffle";
+import { readFileSync } from "fs";
 
-module.exports = {
-  networks: {
-    // Local test network
-    hardhat: {
-      chainId: 1337,
-      // accounts will be generated from hardhad automatically
-    },
-    // Remote network (by infura ethereum)
-    mainnet: {
-      url: "https://mainnet.infura.io/v3/e7aef8e569b14d52b5229976f1e89a5f",
-      accounts: []
-    },
+const privateKey = readFileSync(".secret").toString();
+
+export const networks = {
+  // Local test network
+  hardhat: {
+    chainId: 1337,
+    // accounts will be generated from hardhad automatically
   },
-  solidity: "0.8.4",
+  // Remote network (by infura ethereum)
+  mainnet: {
+    url: "https://mainnet.infura.io/v3/e7aef8e569b14d52b5229976f1e89a5f",
+    accounts: [privateKey], // private key is needed only for remote network in order to connect/interact
+  },
 };
+export const solidity = "0.8.4";
